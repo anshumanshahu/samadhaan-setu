a7157ce49e91d12f0fc55d8d5dc17fb6e3a4c451
Anshuman Shahu <anshulasksahu@gmail.com>
2026-08-09 03:43:02 +0530

    css files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-green": "#0B3D3A",
+        "brand-coral": "#FF6B54",
+      },
+    },
+  },
+};
